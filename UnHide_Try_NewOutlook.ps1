274f7c4a549d8038da2930f7c4816c39f68f7c83
vperalta55 <34689596+vperalta55@users.noreply.github.com>
2024-09-07 08:36:57 -0400

    Add files via upload

---
+++ UnHide_Try_NewOutlook.ps1
@@ -0,0 +1,15 @@
+﻿$keyPath = "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\General"
+$propertyName = "HideNewOutlookToggle"
+
+# Check if the key exists
+if (Test-Path $keyPath) {
+    # Check if the property exists and remove it if it does
+    if ((Get-ItemProperty -Path $keyPath).$propertyName -ne $null) {
+        Remove-ItemProperty -Path $keyPath -Name $propertyName
+        Write-Host "Preview Enabled"
+    } else {
+        Write-Host "Property does not exist"
+    }
+} else {
+    Write-Host "Key does not exist"
+}
